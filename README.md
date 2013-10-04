cd24569819a882a55e76a22495cef0076c92d707
Guy Rozendorn <guy@rzn.co.il>
2013-10-04 10:56:02 +0300

    TRIVIAL Log grep

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Infinidat plug-in for Sublime Text
 ==================================
 
 A collection of commands for Sublime Text that we use here at Infinidat.
-  
+
 Features
 --------
 
--- grep.py
+++ grep.py
@@ -1,24 +1,56 @@
 import sublime, sublime_plugin
 from contextlib import contextmanager
+from itertools import count
+from re import escape
 
 
 def key_func(item):
     return min(item.a, item.b), max(item.a, item.b)
 
 
-def grep_selection(view, edit, invert_selection=True):
+def select_all_lines_containing_current_selection(view):
     view.window().run_command("find_all_under")
     view.run_command("expand_selection", dict(to="line"))
+
+
+def grep_selection(view, edit, invert_selection=True):
+    select_all_lines_containing_current_selection(view)
     if invert_selection:
         view.run_command("invert_selection")
     [view.erase(edit, region) for region in sorted(view.sel(), key=key_func, reverse=True)]
 
 
-@contextmanager
-def stay_in_place(view):
+def exapnd_tracebacks(view):
+    selection = view.sel()
+    regions = list(selection)
+    for region in regions:
+        next_traceback_line_region = view.find(r"(?:\n[^\d].*)+", region.b+1)
+        if next_traceback_line_region.a == next_traceback_line_region.b:
+            break
+        if (next_traceback_line_region.a, next_traceback_line_region.b) == (-1, -1):
+            break
+        selection.add(next_traceback_line_region)
+    view.run_command("expand_selection", dict(to="line"))
+
+
+def log_grep_selection(view, edit, invert_selection=True):
+    select_all_lines_containing_current_selection(view)
+    exapnd_tracebacks(view)
+    if invert_selection:
+        view.run_command("invert_selection")
+    [view.erase(edit, region) for region in sorted(view.sel(), key=key_func, reverse=True)]
+
+
+def get_selected_region(view):
     first_selection = view.sel()[0]
     current_position = min(first_selection.a, first_selection.b), max(first_selection.a, first_selection.b)
     word = view.substr(first_selection)
+    return current_position, word
+
+
+@contextmanager
+def stay_in_place(view):
+    current_position, word = get_selected_region(view)
     all_positions = sorted([(region.a, region.b) for region in view.find_all(word, sublime.LITERAL)])
     index = all_positions.index(current_position)
     yield
@@ -37,3 +69,24 @@ class GrepExcludeCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         grep_selection(self.view, edit, invert_selection=False)
         self.view.sel().clear()
+
+
+class LogGrepCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        _, word = get_selected_region(self.view)
+        if len(word) < 4:
+            sublime.error_message("selection is too short, it will take too long to grep this")
+            return
+        with stay_in_place(self.view):
+            log_grep_selection(self.view, edit)
+
+
+class LogGrepExcludeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        _, word = get_selected_region(self.view)
+        if len(word) < 4:
+            sublime.error_message("selection is too short, it will take too long to grep this")
+            return
+        log_grep_selection(self.view, edit, invert_selection=False)
+        self.view.sel().clear()
+
